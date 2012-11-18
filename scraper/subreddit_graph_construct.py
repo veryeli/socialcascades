d5f14317bb789a60592250527438bd649da5c3fa
Wesley Tansey <wes.tansey@gmail.com>
2012-11-18 14:41:53 -0600

    Fixed bugs in getting posts

--- scraper/subreddit_graph_construct.py
+++ scraper/subreddit_graph_construct.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ReddiWrap import ReddiWrap
 from time import sleep
+import csv
 
 # I guess the thing to do is to index the top several thousand subs (readers geq 10?)
 # then for each sub, gather the name of all commentors in the last say month.
@@ -9,31 +10,35 @@ from time import sleep
 class Graph():
 
 	def __init__(self):
-		self.reddit = ReddiWrap(user_agent='MadeThisUpQuick is trying to map users')
+		self.reddit = ReddiWrap(user_agent='made_this_up_quick')
 		self.min_subreddit_size = 1000
-		self.pages_per_subreddit = 3
+		self.pages_per_subreddit = 5
 		self.indexed_users = set()
 		self.subs = dict()
 		self.get_big_subs()
 		print 'Found {0} subreddits with at least {1} subscribers'.format(len(self.subs), self.min_subreddit_size)
+		self.save_subs()
 		self.graph = np.zeros((len(self.subs), len(self.subs)))
+		self.user_indexes = []
 		while True:
 			print 'Scraping started.'
 			new_users = set()
 			# get all the users in each subreddit that we are not yet tracking
 			print '\tFinding new subreddit users...'
 			for sub in self.subs.keys():
-				print 'Subreddit: {0}'.format(sub)
-				new_users &= self.get_new_users(sub)
+				print '\t\tSubreddit: {0}'.format(sub)
+				new_users |= self.get_new_users(sub)
 			# increment the edge weights based on the new users' subscriptions
 			print '\tUpdating matrix for new users...'
 			for i, user in enumerate(new_users):
+				print '{0}: {1}'.format(i, user)
 				user_subs = self.get_subs(user)
 				self.fill_in(user_subs)
 				self.indexed_users.add(user)
 			# Save every 100 iterations
 			print '\tSaving adjacency matrix...'
 			np.save("adjacencies.npy", self.graph)
+			#self.save_matrix()
 			print 'Finished scraping iteration. Starting over...'
 	
 	def get_big_subs(self):
@@ -54,26 +59,35 @@ class Graph():
 		users = set()
 		sleep(2)
 		posts = self.reddit.get('/r/{0}'.format(sub))
+		if posts is None:
+			sleep(3)
+			posts = self.reddit.get('/r/{0}'.format(sub))
 		for i in range(self.pages_per_subreddit):
 			if self.reddit.has_next():
 				sleep(2)
 				posts += self.reddit.get_next()
 		for post in posts:
-			print '\t\tPost: {0}'.format(post.name)
-			users &= self.get_participants(post)
+			print '\t\t\t{0}'.format(post.permalink)
+			users |= self.get_participants(post)
 		users = [user for user in users if user not in self.indexed_users]
 		return frozenset(users)
 
 	def get_participants(self, post):
 		sleep(2)
-		self.reddit.get(post.url)
-		participants = [comment.author for comment in post.comments]
-		participants.append(post.author)
-		return frozenset(participants)
+		posts = self.reddit.get(post.permalink)
+		if posts is None:
+			sleep(3)
+			posts = self.reddit.get(post.permalink)
+		post = posts[0]
+		participants = [comment.author for comment in post.comments if comment.author != "[deleted]"]
+		if(post.author != "[deleted]"):
+			participants.append(post.author)
+		result = frozenset(participants)
+		return result
 
 	def get_subs(self, user):
 		user_subs = [comment.subreddit for comment in self.reddit.get_user_comments(user, 'new')]
-		user_subs &= [post.subreddit for post in self.reddit.get_user_posts(user, 'new')]
+		user_subs += [post.subreddit for post in self.reddit.get_user_posts(user, 'new')]
 		return frozenset(user_subs)
 
 	def fill_in(self, user_subs):
@@ -81,6 +95,16 @@ class Graph():
 			for sub2 in user_subs:
 				if self.subs.has_key(sub) and self.subs.has_key(sub2):
 					self.graph[self.subs[sub], self.subs[sub2]] += 1
+
+	def save_subs(self):
+		writer = csv.writer(open('subreddits.csv', 'wb'))
+		for key, value in self.subs.items():
+			writer.writerow([key, value])
+
+	def save_matrix(self):
+		writer = csv.writer(open('adjacencies.csv', 'wb'))
+		for i in range(0,len(self.subs)):
+			writer.writerow(self.graph[i,:])
 
 def main():
 	g = Graph()
