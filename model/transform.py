3ec3647fd18d40f896b8571b9b7686d3cc6d3d31
Wesley Tansey <wes.tansey@gmail.com>
2012-12-07 10:30:27 -0600

    Added percent fold validation. Removed extraneous prints

--- model/transform.py
+++ model/transform.py
@@ -1,21 +1,31 @@
 from get_samples import get_samples
-
+import sys
 
 def transform_to_netinf(fname):
-	f = open(fname+'.netinf', 'w')
+	f = open(fname.replace('.csv','.netinf'), 'wb')
 	for sample in get_samples(fname):
 		numvars= sample.shape[1]
 	for i in range(numvars):
 		f.write("%s,%s\n" %(i,i))
+	total = 0
 	for sample in get_samples(fname):
 		numvars= sample.shape[1]
 		netinf = {}
+		print total
 		for i in range(sample.shape[0]):
 			for j in range(sample.shape[1]):
-				if sample[i][j] == 1:
-					if i not in netinf.keys():
-						netinf[j] = i 
-
+				if sample[i,j] == 1:
+					if j not in netinf:
+						netinf[j] = i
 		f.write('\n')
 		for item in netinf.keys():
 			f.write('%s,%s;' % (item, netinf[item]))
+		total += 1
+
+if __name__ == "__main__":
+	prefix = sys.argv[1]
+	splits = int(sys.argv[2])
+	for i in range(splits):
+		print "Converting {0}".format(i)
+		transform_to_netinf(prefix + "_test" + str(i) + ".csv")
+		transform_to_netinf(prefix + "_train" + str(i) + ".csv")
